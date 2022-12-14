b1e355d4ea02b1692b86665bae040f1ca44b2027
real.tesla@gmail.com <real.tesla@gmail.com>
2022-12-14 22:51:34 +0300

    json

--- main.py
+++ main.py
@@ -1,35 +1,45 @@
 import pyscreenshot as ss
 import hashlib
-import time
 import json
+# from PIL import Image
 
 from pynput import keyboard
+from threading import Thread
 
 # import subprocess
 
 import win32api
 import win32con
 
-BOX = (1090, 680, 1788, 842)
+# BOX = (1090, 680, 1788, 842)
+BOX = (1090, 684, 1788, 846)
 bw, bh = (344, 76)
 sl, st = (354, 86)
 
+
+# s = Image.open('last.png')
+# lt = s.crop(box=(0, 0, bw, bh))
+# rt = s.crop(box=(sl, 0, sl + bw, bh))
+# lb = s.crop(box=(0, st, bw, st + bh))
+# rb = s.crop(box=(sl, st, sl + bw, st + bh))
+#
+# hashes = []
+# for im in [lt, rt, lb, rb]:
+#     p = im.getpixel((0, 0))
+#     print(p)
+#     if p[1] < 200:
+#         hashes.append(hashlib.sha1(im.tobytes()).hexdigest())
+# print(sorted(hashes))
+#
+# exit(1)
+
 def click(x, y):
     win32api.SetCursorPos((x, y))
-    # win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-    # win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
 
-click(1090, 680)
-exit(1)
-
-FILE = 'bads.json'
-with open(FILE, "r") as f:
-    bads = json.load(f)
-
-
-def my_write():
-    start = time.time()
+def m_get_hashes():
     s = ss.grab(bbox=BOX)
     lt = s.crop(box=(0, 0, bw, bh))
     rt = s.crop(box=(sl, 0, sl + bw, bh))
@@ -37,42 +47,88 @@ def my_write():
     rb = s.crop(box=(sl, st, sl + bw, st + bh))
 
     hashes = []
+    all_hashes = []
     for im in [lt, rt, lb, rb]:
-        print(im.getpixel((0, 0)))
-        if (len(set(im.getpixel((0, 0))))) <= 1:
-            hashes.append(hashlib.sha1(im.tobytes()).hexdigest())
-    hashes.sort()
+        p = im.getpixel((0, 0))
+        h = hashlib.sha1(im.tobytes()).hexdigest()
+        all_hashes.append(h)
+
+        if p[1] < 200:
+            hashes.append(h)
+    # s.save('tmp/' + '_'.join(all_hashes) + '.png')
+    return sorted(hashes), all_hashes
+
+
+def m_search():
+    hashes, all = m_get_hashes()
+    if len(hashes) != 4:
+        print(f'search {len(hashes)}')
+        return
+
+    for h in bads:
+        t = set(hashes).difference(set(h))
+        if len(t) == 1:
+            print(f"found {t}")
+            if all[0] in t:
+                print('lt')
+                click(BOX[0], BOX[1])
+            if all[1] in t:
+                print('rt')
+                click(BOX[0]+sl, BOX[1])
+            if all[2] in t:
+                print('lb')
+                click(BOX[0], BOX[1]+st)
+            if all[3] in t:
+                print('rb')
+                click(BOX[0]+sl, BOX[1]+st)
+            return
+
+    print(f"not found", all)
+    win32api.SetCursorPos((int((BOX[0] + BOX[2])/2), int((BOX[1] + BOX[3])/2)))
+
+
+def m_write():
+    hashes, _ = m_get_hashes()
+    if len(hashes) != 3:
+        print(f'save {len(hashes)}')
+        return
 
     if hashes not in bads:
         bads.append(hashes)
-        with open(FILE, "w") as f:
-            json.dump(bads, f)
-        print("saved")
+        with open(FILE, "w") as file:
+            json.dump(bads, file)
+        print("saved", len(bads))
+    else:
+        print("exist")
 
-    for h in bads:
-        print("> ", set({'be2df90210d335487dd56eab8ab2c4f11f67585e',
-                         '85fa5c89746f2780c77562f1e81294adc200ffc5',
-                         '5c76e325e8932b72ec6f78b96b186d8387712145',
-                         '58a0c0672baecaf22d3b1b2b4eded2df9b811f57'}).difference(set(h)))
-    print(bads)
 
-    # s.save('test.png')
-    # lt.save('lt.png')
-    # rt.save('rt.png')
-    # lb.save('lb.png')
-    # rb.save('rb.png')
-    print(time.time() - start)
-
+FILE = 'bads.json'
+try:
+    with open(FILE, "r") as f:
+        bads = json.load(f)
+except FileNotFoundError:
+    bads = []
 
 pressed = set()
 
 COMBINATIONS = [
     {
         "keys": [
-            {keyboard.Key.cmd, keyboard.KeyCode(char="w")},
-            {keyboard.Key.cmd, keyboard.KeyCode(char="W")},
+            {keyboard.Key.cmd, keyboard.KeyCode(char="A")},
+            {keyboard.Key.cmd, keyboard.KeyCode(char="a")},
+            {keyboard.Key.cmd, keyboard.KeyCode(char="Ф")},
+            {keyboard.Key.cmd, keyboard.KeyCode(char="ф")},
         ],
-        "command": my_write,
+        "command": m_search,
+    },
+    {
+        "keys": [
+            {keyboard.Key.cmd, keyboard.KeyCode(char="S")},
+            {keyboard.Key.cmd, keyboard.KeyCode(char="s")},
+            {keyboard.Key.cmd, keyboard.KeyCode(char="Ы")},
+            {keyboard.Key.cmd, keyboard.KeyCode(char="ы")},
+        ],
+        "command": m_write,
     },
 ]
 
@@ -81,12 +137,13 @@ COMBINATIONS = [
 #     subprocess.Popen(s)
 
 def on_press(key):
+    if key in pressed: return
+
     pressed.add(key)
-    print(pressed)
     for c in COMBINATIONS:
         for keys in c["keys"]:
             if keys.issubset(pressed):
-                c["command"]()
+                Thread(target=c["command"]).start()
 
 
 def on_release(key):
@@ -94,5 +151,5 @@ def on_release(key):
         pressed.remove(key)
 
 
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+with keyboard.Listener(on_press=on_press, on_release=on_release, suppress=True) as listener:
     listener.join()
